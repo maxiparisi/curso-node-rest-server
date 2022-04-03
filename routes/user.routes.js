c93eb3184cd068e53b40f048e7dbc86224f67787
Maximiliano Parisi <maximiliano.parisi@gire.com>
2022-04-03 03:01:33 -0300

    creamos una validacion particular para el rol, verificando q exista en la BBDD. Se crea para eso Schema/modelo para mongo de ROL y se consulta en la validacion si existe el rol en la BBDD

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,7 @@ const { Router} = require('express');
 const { check } = require('express-validator');
 const { usuariosGet, usuariosPost, usuariosPut, usuariosDelete, usuariosPatch } = require('../controllers/usuario.controller');
 const { validarCampos } = require('../middlewares/validar-campos');
+const Role = require('../models/role');
 
 const router = Router();
 
@@ -12,7 +13,13 @@ router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'La contraseña es obligatoria y debe tener al menos 6 caracteres').isLength({min: 6}),
     check('correo', 'El correo no es válido').isEmail(),
-    check('rol', 'No es un rol válido').isIn('ADMIN_ROLE', 'USER_ROLE'),
+    // check('rol', 'No es un rol válido').isIn('ADMIN_ROLE', 'USER_ROLE'),
+    check('rol').custom( async(rol = '') => {
+        const existeRol = await Role.findOne({rol}); //que exista el rol en la base de datos en los documentos de roles
+        if(!existeRol) {
+            throw new Error(`El rol ${rol} no existe en la BBDD`);
+        }
+    }),
     validarCampos
 ] ,usuariosPost)
 
